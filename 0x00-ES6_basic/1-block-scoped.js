1a3b8c28b742d8d4ed11f1140f508029128bb2f5
vinsky001 <ekibet544@gmail.com>
2023-09-25 14:09:36 +0000

    Modified the variables inside the function taskBlock so that the variables aren’t overwritten

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+    
+    if (trueOrFalse) {
+        const task = true; // eslint-disable-line
+        const task2 = false; // eslint-disable-line
+    }
+    return [task, task2];
+}
