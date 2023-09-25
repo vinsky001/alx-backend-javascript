ebfa972926b93df030a7671bb8374fbb4c957efc
vinsky001 <ekibet544@gmail.com>
2023-09-25 22:17:08 +0000

    Header file

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,6 +1,6 @@
-export default function  getSanFranciscoDescription() {
-    const year = 2017;
-    const budget = {
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
     income: '$119,868',
     gdp: '$154.2 billion',
     capita: '$178,479',
