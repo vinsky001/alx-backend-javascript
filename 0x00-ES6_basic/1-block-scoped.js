0a60ba9cfd65b42e597ab05da70f71ef2f41abf2
vinsky001 <ekibet544@gmail.com>
2023-09-26 07:58:03 +0000

    Eslint indentation fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
-  const task2 = tr 
-  if (trueOrFalse) {
-   const task = true; // eslint-disable-line
-   const task2 = false; // eslint-disable-line
-  }
-    return [task, task2];
-}
+  const task2 = true;
 
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line
+    const task2 = false; // eslint-disable-line
+  }
+
+  return [task, task2];
+}
