a4a2878b4fce58e5e6a617f4114832c3857c836f
vinsky001 <ekibet544@gmail.com>
2023-09-28 18:52:48 +0000

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super()._sqft = sqft;
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
