afe9afacb16306113dc63104150767fefa2797e5
vinsky001 <ekibet544@gmail.com>
2023-09-28 19:30:31 +0000

    method should return a new object of the class

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const Obj = this.constructor[Symbol.species] || this.constructor;
+    const newObj = new Obj();
+    return newObj;
+  }
+}
