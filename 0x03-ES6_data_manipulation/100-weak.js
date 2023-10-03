a9ec947b61cd43825b0b00a3146dfefd43f3821e
vinsky001 <ekibet544@gmail.com>
2023-10-03 21:27:02 +0000

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap, queryAPI };
