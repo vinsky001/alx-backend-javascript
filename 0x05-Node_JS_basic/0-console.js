d624d54192cab684e6576fdcf84df64738cfba89
vinsky001 <ekibet544@gmail.com>
2023-11-21 08:26:56 +0000

    function named displayMessage that prints in STDOUT the string argument

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+function displayMessage(message) {
+    process.stdout.write(message);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const displayMessage = require('./0-console');
+
+displayMessage("Hello NodeJS!");
